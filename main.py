3ec33aedcfeedf536e9877dada0464e6fc69aa84
Milan Thapa <45590593+milanthapaa@users.noreply.github.com>
2022-05-24 18:03:32 +0545

    update download destination

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def download_yt_playlist():
     playlist = Playlist("https://www.youtube.com/playlist?list=PLrzb5ti9EMxb43gxMUI1U9epbSO2pyLPg")
     print(f"Downloading: {playlist.title}")
     for video in playlist.videos:
-        video.streams.get_highest_resolution().download()
+        video.streams.get_highest_resolution().download(output_path=r"C:\Users\user\Downloads")
 
 
 if __name__ == '__main__':

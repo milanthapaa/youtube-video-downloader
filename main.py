20707a37fe0f87eb86f2794730e4c25f4c491bd6
Milan Thapa <45590593+milanthapaa@users.noreply.github.com>
2022-05-24 17:47:25 +0545

    function to download playlist added

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from pytube import YouTube
+from pytube import Playlist
+
+
+def download_yt_playlist():
+    playlist = Playlist("https://www.youtube.com/playlist?list=PLrzb5ti9EMxb43gxMUI1U9epbSO2pyLPg")
+    print(f"Downloading: {playlist.title}")
+    for video in playlist.videos:
+        video.streams.first().download()
+
 
 if __name__ == '__main__':
-    pass
+    download_yt_playlist()
